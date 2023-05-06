01e04b123a765c2066da73f9ce885c7b6e10fa78
unknown <heart.of.bass@gmail.com>
2023-05-06 12:15:53 +0300

    Changes according PEP8.

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось задеплоить это в облако!!!1адин')
+    return HttpResponse('У меня получилось задеплоить это!')
 
 
 def second_page(request):
